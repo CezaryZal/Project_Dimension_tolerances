d6ef8a1edc1ce853e5c43d5abb106c21948ca226
Cezary Zalewski <czarek0821@gmail.com>
2019-04-26 10:32:45 +0200

    add method connect and disconnect with database

--- src/DbAdapter.java
+++ src/DbAdapter.java
@@ -1,2 +1,46 @@
+import java.sql.*;
+
 public class DbAdapter {
+
+    String jdbUrl = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    String username = "root";
+    String password = "SQLSerwer2019";
+
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+
+    /**
+     * Connect to database
+     */
+
+    public Connection connect() {
+        try {
+            connection = DriverManager.getConnection(jdbUrl, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    /**
+     * Disconnect to database
+     */
+
+    public void disconnect (){
+        try {
+            if (connection != null){
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+        } catch (Exception e){
+            e.getMessage();
+        }
+    }
 }
